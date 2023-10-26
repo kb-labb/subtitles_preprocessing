79475de4f5d27840b6c60b2ba57bb63d3f43b5f1
Leonora Vesterbacka <leonoravesterbacka@gmail.com>
2023-10-26 15:07:06 +0100

    splitting audio into 30s, trying language detect

--- 01_extract_srt_mp3_wav.py
+++ 01_extract_srt_mp3_wav.py
@@ -1,13 +1,26 @@
 import subprocess as sp
-import os 
+import os
+from os import makedirs 
 import argparse
 import time
 
 
 parser = argparse.ArgumentParser(formatter_class = argparse.RawDescriptionHelpFormatter)
-parser.add_argument("-o", "--output_path")
-parser.add_argument("-i", "--rootdirectory")
-parser.add_argument("-f", "--sound_format", default = "mp3")
+
+parser.add_argument(
+    "--o",
+    type=str,
+    default="results",
+    help="Directory where wav and srt files end up.",
+)
+parser.add_argument(
+    "--i",
+    type=str,
+    default="input",
+    help="Directory containing mp4 files.",
+)
+
+parser.add_argument("-f", "--sound_format", default = "wav")
 args = vars(parser.parse_args())
 
 def check_and_extract(videofile, file, savedir, format):
@@ -22,29 +35,31 @@ def check_and_extract(videofile, file, savedir, format):
     if len(swe_idx) >= 1:
         if os.path.isfile(f"{(os.path.join(savedir, file))[:-4]}.srt") == False:
             # Save subtitle in srt format
-            out = sp.run(['ffmpeg','-i', videofile, '-map', f's:{swe_idx[0]}', '-f','srt', f'{savedir}/{file[:-4]}.srt'], stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True)
+            out = sp.run(['ffmpeg','-i', videofile, '-map', f's:{swe_idx[0]}', '-f','srt', f'{savedir}/{file[:-4]}/file.srt'], stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True)
         else:
             pass
         if format == "mp3":
         # Save sound in mp3 format
-            out = sp.run(['ffmpeg', '-i' , videofile, '-acodec',  'libmp3lame', f'{savedir}/{file[:-4]}.mp3'])
+            out = sp.run(['ffmpeg', '-i' , videofile, '-acodec',  'libmp3lame', f'{savedir}/{file[:-4]}/file.mp3'])
         elif format == "wav":   
-            out = sp.run(['ffmpeg', '-i' , videofile, '-acodec',  'pcm_s16le', '-ac', '1', '-ar', '16000', f'{savedir}/{file[:-4]}.wav'])
+            out = sp.run(['ffmpeg', '-i' , videofile, '-acodec',  'pcm_s16le', '-ac', '1', '-ar', '16000', f'{savedir}/{file[:-4]}/file.wav'])
     else:
         print(f"Swedish subtitles are not available for {file}")
         
 
 def main():
 
-    rootdir = args["rootdirectory"]
-    savedir = args["output_path"]
+    rootdir = args["i"]
+    savedir = args["o"]
     format = args["sound_format"]
     
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
             videofile_path = os.path.join(subdir, file)
-            video_savedir = os.path.join(savedir, file)
-            if os.path.isfile(f"{video_savedir[:-4]}.{format}") == False:
+            if not os.path.exists(os.path.join(savedir, file[:-4])):
+                makedirs(os.path.join(savedir, file[:-4]))
+            video_savedir = os.path.join(savedir, file[:-4])
+            if os.path.isfile(f"{video_savedir}/file.{format}") == False:
                 check_and_extract(videofile_path, file, savedir, format)
             else:
               print(f"Skipping, {file[:-4]}.{format} already exists.")
---
+++ 03_preprocess_split_audio.py
@@ -0,0 +1,117 @@
+import argparse
+import os
+import sys
+import pandas as pd
+import pdb
+from multiprocessing import Pool
+from tqdm import tqdm
+from pydub import AudioSegment
+import concurrent.futures
+import logging
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+import whisper
+
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+logging.info("Starting")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--data",
+    type=str,
+    default="results",
+    help="Directory containing subdirectories with audio and srt files.",
+)
+
+parser.add_argument(
+    "--output",
+    type=str,
+    default="chunks",
+    help="Directory where the resulting wav file chunks should be saved.",
+)
+
+parser.add_argument(
+    "--processes",
+    type=int,
+    default=2,
+    help="Number of processes to use for multiprocessing.",
+)
+
+parser.add_argument(
+    "--parquet_dir",
+    type=str,
+    default="subs_preprocessed",
+    help="Name of directory where parquet file(s) are stored.",
+)
+args = parser.parse_args()
+
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
+processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
+
+output_dir = args.output
+
+parquet_files = [d for d in os.listdir(args.parquet_dir)]    
+
+def split_audio(audio_df : pd.DataFrame):                    
+    f = audio_df.iloc[0, audio_df.columns.get_loc("program")]
+    audio_file_name = audio_df.iloc[0, audio_df.columns.get_loc("audio")]
+    audio_path = f"{args.data}/{f}/{audio_file_name}"
+
+    audio = AudioSegment.from_wav(audio_path)
+    os.makedirs(output_dir, exist_ok=True)
+    filenames = []
+    for i, row in audio_df.iterrows():
+        audio_chunk = audio[row["start_bucket"] : row["end_bucket"]]
+        os.makedirs(os.path.join(output_dir, row['program']), exist_ok=True)
+        filename = f"{output_dir}/{row['program']}/{row['observation_nr']}.wav"
+        #detect_language(filename) #detect language doesnät work, wither whisper-tiny sucks or I'm not doing it correctly
+        audio_chunk.export(filename, format="wav")
+        filenames.append(filename)
+
+    return filenames
+
+def detect_language(audio):
+    audio = whisper.load_audio(audio)
+    audio = whisper.pad_or_trim(audio)
+    input_features = processor(audio, sampling_rate=16000, return_tensors="pt").input_features
+    pred_tokens = model.generate(input_features, max_new_tokens=448)
+    pred_text = processor.batch_decode(pred_tokens, skip_special_tokens=True)
+    pred_language = processor.batch_decode(pred_tokens[:, 1:2], skip_special_tokens=False)
+    #
+    print(pred_text)
+    print(pred_language)
+
+# Multiple parquet files do not work yet, we overwrite the same metadata.csv file, but it is easy to fix.
+metadata_df = pd.DataFrame()
+for parquet_file in tqdm(parquet_files):
+    logging.info(f"Processing parquet file {parquet_file}")
+
+    df_subs = pd.read_parquet(args.parquet_dir + "/" + parquet_file)
+    df_subs["bucket_filename"] = (
+            df_subs["observation_nr"].astype(str) + ".wav"
+    )
+
+    # Keep only first obs in each observation_nr group
+    df_subs_unique = df_subs.drop_duplicates(["observation_nr", "audio"], keep="first")
+    df_subs_unique = df_subs_unique.drop(df_subs_unique.tail(1).index) 
+    
+    audio_groups = df_subs_unique[["program", "audio", "start_bucket", "end_bucket", "bucket_filename",
+                                   "observation_nr"]].groupby("audio")
+    df_list = [audio_groups.get_group(df) for df in audio_groups.groups]
+    
+    logging.info("Splitting audio")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        out_filenames = list(tqdm(executor.map(split_audio, df_list), total=len(df_list)-1))
+
+    df_subs_unique = df_subs_unique.copy()
+    df_subs_unique["bucket_path"] = [bucket_path for program_paths_list in out_filenames for bucket_path in program_paths_list]
+
+    result = df_subs_unique[['bucket_filename', 'text_timestamps_bucket']].rename(columns={'bucket_filename': 'file_name',
+                                                                                           'text_timestamps_bucket':
+                                                                                               'transcription'})
+    result.to_csv(f"{output_dir}/{df_subs['program'][0]}/metadata.csv", index=False)
+
+logging.info("Done")
