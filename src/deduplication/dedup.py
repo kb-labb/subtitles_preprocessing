f3ae2a4ba7606ade632d1ff140d52decf2c51f10
Robin Kurtz <robin.kurtz@kb.se>
2023-12-09 20:08:01 +0100

    potential bug fix and no more deque

--- src/deduplication/dedup.py
+++ src/deduplication/dedup.py
@@ -10,6 +10,9 @@ import re
 import shutil
 import time
 from datasets import load_from_disk
+import multiprocessing as mp
+from functools import partial
+from collections import deque
 
 MAX_LIST_SIZE = 100_000
 # gpt2 tokenzer eos-token
@@ -55,7 +58,24 @@ def save_subs_as_hf_dataset(in_data, out_data):
 
 
 # %%
-def save_subs_as_txt(in_data, out_data):
+def read_write_subs(fn, fn_out):
+    data = []
+    index = []
+    subs = pysrt.open(fn)
+    # suffix = fn.split("/")[-2]
+    # with open(f"{fn_out}/subs.{suffix}", "w") as fout, open(
+    #     f"{fn_out}/index.{suffix}", "w"
+    # ) as findex:
+    if True:
+        for sub in subs:
+            data.append(sub.text_without_tags.replace("\n", NEWLINE))
+            index.append((fn, sub.index))
+            # print(sub.text_without_tags.replace("\n", NEWLINE), file=fout)
+            # print((fn, sub.index), file=findex)
+    return data, index
+
+
+def save_subs_as_txt(in_data, out_data, n_processes):
     try:
         os.makedirs(out_data, exist_ok=False)
     except FileExistsError:
@@ -72,19 +92,141 @@ def save_subs_as_txt(in_data, out_data):
     fn_out = f"{out_data}/subs.txt"
     fn_index = f"{out_data}/index.txt"
     file_names = glob.iglob(f"{in_data}/**/file.srt", recursive=True)
-    for fn in tqdm(file_names):
-        data = []
-        index = []
-        subs = pysrt.open(fn)
+    with open(fn_out, "w") as fout, open(fn_index, "w") as findex:
+        my_fun = partial(read_write_subs, fn_out=out_data)
+        with mp.Pool(processes=n_processes) as pool:
+            results = pool.imap(my_fun, tqdm(file_names))
+            # for _ in results:
+            #     pass
+            # results = tqdm(pool.map(read_write_subs, (file_names)))
+            for data, index in results:
+                for d in data:
+                    print(d, file=fout)
+                for i in index:
+                    print(i, file=findex)
+
+
+def stupid_dedup_dequeu(in_data, out_data):
+    dups = 0
+    total = 0
+    seen = set()
+    file_names = glob.iglob(f"{in_data}/**/file.srt", recursive=True)
+
+    n = 4
+    for fn in file_names:
+        fn_out = "tmp.srt"
+        fuse_subtitles.fuse_subtitles(fn, fn_out)
+        subs = pysrt.open(fn_out)
+
+        prev_candidate = deque([False] * n, maxlen=n)
+        prev_items = deque([], maxlen=n)
+
+        program_id = fn.split("/")[-2]
+        xa, channel_1, channel_2, date = program_id.split("_")[:4]
+        year, month, day = date.split("-")
+        out_path = "/".join([xa, channel_1, channel_2, year, month, day])
+        os.makedirs(f"{out_data}/{out_path}/{program_id}", exist_ok=True)
+        fout = pysrt.SubRipFile(path=f"{out_data}/{out_path}/{program_id}/file.srt")
+
+        duplicate = 0
         for sub in subs:
-            data.append(sub.text_without_tags.replace("\n", NEWLINE))
-            index.append((fn, sub.index))
-        with open(fn_out, "a") as fout:
-            for d in data:
-                print(d, file=fout)
-        with open(fn_index, "a") as fout:
-            for i in index:
-                print(i, file=fout)
+            prev_items.append(sub)
+            text = hash(sub.text_without_tags)
+            if text not in seen:
+                seen.add(text)
+                prev_candidate.append(False)
+            else:
+                prev_candidate.append(True)
+
+            if all(prev_candidate):
+                duplicate = n
+            else:
+                duplicate = max((0, duplicate - 1))
+
+            if duplicate > 0:
+                dups += 1
+                prev_items[0].text = "<duplicate>" + prev_items[0].text + "</duplicate>"
+            fout.append(prev_items[0])
+            total += 1
+            # print(f"{dups} / {dups} ≃ {dups/total:.2%}", end="\r")
+        # get the rest out of the deque
+        while prev_items:
+            if duplicate > 0:
+                dups += 1
+                prev_items[0].text = "<duplicate>" + prev_items[0].text + "</duplicate>"
+            duplicate = max((0, duplicate - 1))
+            fout.append(prev_items.popleft())
+
+        fout.save()
+        try:
+            print(f"{dups} / {dups} ≃ {dups/total:.2%}", end="\r")
+        except ZeroDivisionError:
+            print("empty")
+    try:
+        print("Finally...")
+        print(f"{dups} / {dups} ≃ {dups/total:.2%}", end="\n")
+    except ZeroDivisionError:
+        print("empty")
+
+
+def stupid_dedup_counting(in_data, out_data):
+    dups = 0
+    total = 0
+    seen = set()
+    file_names = glob.iglob(f"{in_data}/**/file.srt", recursive=True)
+
+    n = 4
+
+    for fn in file_names:
+        fn_out = "tmp.srt"
+        fuse_subtitles.fuse_subtitles(fn, fn_out)
+        subs = pysrt.open(fn_out)
+
+        program_id = fn.split("/")[-2]
+        xa, channel_1, channel_2, date = program_id.split("_")[:4]
+        year, month, day = date.split("-")
+        out_path = "/".join([xa, channel_1, channel_2, year, month, day])
+        os.makedirs(f"{out_data}/{out_path}/{program_id}", exist_ok=True)
+        fout = pysrt.SubRipFile(path=f"{out_data}/{out_path}/{program_id}/file.srt")
+
+        duplicate = 0
+        duplicate_candidate = 0
+        for s_i, sub in enumerate(subs):
+            text_hash = hash(sub.text_without_tags)
+            if text_hash not in seen:
+                seen.add(text_hash)
+                duplicate_candidate = 0
+            else:
+                duplicate_candidate += 1
+
+            if duplicate_candidate >= n:
+                duplicate = n
+            else:
+                duplicate -= 1
+
+            if duplicate > 0:
+                dups += 1
+                subs[s_i - n].text = "<duplicate>" + subs[s_i - n].text + "</duplicate>"
+            fout.append(subs[s_i - n])
+            total += 1
+        # get the rest
+        for s_i in range(n, 0, -1):
+            if duplicate > 0:
+                dups += 1
+                subs[-s_i].text = "<duplicate>" + subs[-s_i].text + "</duplicate>"
+            duplicate -= 1
+            fout.append(subs[-s_i].popleft())
+
+        fout.save()
+        try:
+            print(f"{dups} / {total} ≃ {dups/total:.2%}", end="\r")
+        except ZeroDivisionError:
+            print("empty")
+    try:
+        print("\nFinally...")
+        print(f"{dups} / {total} ≃ {dups/total:.2%}", end="\n")
+    except ZeroDivisionError:
+        print("empty")
 
 
 # %%
@@ -167,15 +309,22 @@ def mark_duplicates_hf(dup_hf_fn, output_dir):
 # %%
 # save_subs_as_txt("/home/robkur/data/delat/undertexter/", "hej")
 if __name__ == "__main__":
-    # save_subs_as_txt("/home/robkur/data/delat/srt_only/", "srt_only")
+    start = time.time()
+    # stupid_dedup("/home/robkur/data/delat/srt_only/tv4/tv4/2023/10/", "test")
+    stupid_dedup_counting("/home/robkur/data/delat/srt_only/", "srt_only_dedup")
+    # save_subs_as_txt("/home/robkur/data/delat/srt_only/", "srt_only", mp.cpu_count())
+    # save_subs_as_txt(
+    #     "/home/robkur/data/delat/srt_only/tv4/tv4/2023/10/", "test", mp.cpu_count()
+    # )
     # save_subs_as_hf_dataset("/home/robkur/data/delat/srt_only/", "srt_only_hf")
 
     # save_subs_as_hf_dataset(
     #     "/home/robkur/data/delat/srt_only/tv4/tv4/2023/10", "test_hf"
     # )
-    mark_duplicates_hf(
-        "/home/robkur/workspace/text-dedup/output/suffix_array/test_data/",
-        "test_deduped",
-    )
+    # mark_duplicates_hf(
+    #     "/home/robkur/workspace/text-dedup/output/suffix_array/test_data/",
+    #     "test_deduped",
+    # )
 
     # mark_duplicates("/home/robkur/workspace/subtitles_preprocessing/hej/subs.dedup-marked", "/home/robkur/workspace/subtitles_preprocessing/hej/index.txt", "./tmp")
+    print(f"took {time.time() - start:.2f} seconds")
