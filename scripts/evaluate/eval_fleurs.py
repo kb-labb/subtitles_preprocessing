fdfcd8c67e17acfd22ed43e8d4f64656951fcbad
Lauler <fatonr@gmail.com>
2025-01-08 13:53:10 +0100

    Eval 1 model at a time

--- scripts/evaluate/eval_fleurs.py
+++ scripts/evaluate/eval_fleurs.py
@@ -8,7 +8,6 @@ import unicodedata
 import pandas as pd
 import soundfile as sf
 import torch
-import torchaudio
 from datasets import load_dataset
 from jiwer import wer
 from nltk.translate.bleu_score import SmoothingFunction, sentence_bleu
@@ -17,8 +16,8 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 from transformers.pipelines.pt_utils import KeyDataset
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument("--model_kblab", type=str, default="whisper-medium")
-argparser.add_argument("--model_openai", type=str, default="whisper-medium")
+argparser.add_argument("--model", type=str, default="whisper-large-9000")
+argparser.add_argument("--organization", type=str, default="KBLab")
 argparser.add_argument("--split", type=str, default="validation")
 argparser.add_argument("--results_dir", type=str, default="results")
 args = argparser.parse_args()
@@ -73,26 +72,16 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    args.model_kblab,
+    args.model,
     cache_dir="cache",
     torch_dtype=torch_dtype,
     attn_implementation="flash_attention_2",
 )
 
-model_openai = AutoModelForSpeechSeq2Seq.from_pretrained(
-    args.model_openai,
-    cache_dir="cache",
-    torch_dtype=torch_dtype,
-    attn_implementation="flash_attention_2",
-)
 model.to("cuda")
-model_openai.to("cuda")
-
-model_openai.eval()
 model.eval()
 
-processor = AutoProcessor.from_pretrained(args.model_kblab, cache_dir="cache")
-processor_openai = AutoProcessor.from_pretrained(args.model_openai, cache_dir="cache")
+processor = AutoProcessor.from_pretrained(args.model, cache_dir="cache")
 
 gen_kwargs = {
     "task": "transcribe",
@@ -112,19 +101,6 @@ pipe = pipeline(
     generate_kwargs=gen_kwargs,
 )
 
-pipe_openai = pipeline(
-    "automatic-speech-recognition",
-    model=model_openai,
-    tokenizer=processor_openai.tokenizer,
-    feature_extractor=processor_openai.feature_extractor,
-    torch_dtype=torch_dtype,
-    device=device,
-    batch_size=32,
-    return_timestamps=False,
-    chunk_length_s=30,
-    generate_kwargs=gen_kwargs,
-)
-
 fleurs = load_dataset("google/fleurs", "sv_se", split=args.split, cache_dir="cache")
 
 
@@ -132,17 +108,9 @@ output_texts = []
 for out in pipe(KeyDataset(fleurs, "audio")):
     output_texts.append(out["text"])
 
-output_texts_openai = []
-for out in pipe_openai(KeyDataset(fleurs, "audio")):
-    output_texts_openai.append(out["text"])
-
 
 wer_list = [
     wer(normalize_text(fleurs["transcription"][i]), normalize_text(output_texts[i]))
-    for i in range(len(fleurs))
-]
-wer_list_openai = [
-    wer(normalize_text(fleurs["transcription"][i]), normalize_text(output_texts_openai[i]))
     for i in range(len(fleurs))
 ]
 
@@ -150,44 +118,34 @@ bleu_list = [
     calculate_bleu(normalize_text(fleurs["transcription"][i]), normalize_text(output_texts[i]))
     for i in range(len(fleurs))
 ]
-bleu_list_openai = [
-    calculate_bleu(
-        normalize_text(fleurs["transcription"][i]), normalize_text(output_texts_openai[i])
-    )
-    for i in range(len(fleurs))
-]
 
 # Calculate average WER
-kblab_wer = sum(wer_list) / len(wer_list)
-openai_wer = sum(wer_list_openai) / len(wer_list_openai)
+model_wer = sum(wer_list) / len(wer_list)
 
 # Calculate average BLEU
-kblab_bleu = sum(bleu_list) / len(bleu_list)
-openai_bleu = sum(bleu_list_openai) / len(bleu_list_openai)
+model_bleu = sum(bleu_list) / len(bleu_list)
 
 print(
     (
-        f"Average WER for KBLab model: {kblab_wer} \n"
-        f"Average WER for OpenAI model: {openai_wer} \n"
-        f"Average BLEU for KBLab model: {kblab_bleu} \n"
-        f"Average BLEU for OpenAI model: {openai_bleu} \n"
+        f"Average WER for KBLab model: {model_wer} \n"
+        f"Average BLEU for KBLab model: {model_bleu} \n"
     )
 )
 
 
 results_dict = {
-    "WER": [kblab_wer, openai_wer],
-    "BLEU": [kblab_bleu, openai_bleu],
+    "WER": [model_wer],
+    "BLEU": [model_bleu],
 }
 
 df = pd.DataFrame(results_dict)
-df["organization"] = ["KBLab", "OpenAI"]
-df["model"] = [args.model_kblab, args.model_openai]
+df["organization"] = args.organization
+df["model"] = args.model
 df["eval_dataset"] = "fleurs_" + args.split
 
 os.makedirs(args.results_dir, exist_ok=True)
 df[["model", "eval_dataset", "organization", "WER", "BLEU"]].to_csv(
-    f"{args.results_dir}/results_{args.model_kblab}_{args.split}.csv",
+    f"{args.results_dir}/results_{args.model}_{args.split}.csv",
     index=False,
 )
 
